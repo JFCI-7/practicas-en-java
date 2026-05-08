2a8a4cf0da750c2b8a6fa3894e7f24d067840bd4
Jesus Islas <jesusfernando7ci@gmail.com>
2026-05-07 18:07:48 -0600

    Add additional separator line in main method

--- src/com/centuriati/jfci/basico/practicas/Main.java
+++ src/com/centuriati/jfci/basico/practicas/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String agrs[]) {
 		System.out.println("============================================================");
+		System.out.println("============================================================");
 	}
 
 }
